c28912375849ea38bd9a114ae432736d518e7d65
AndrewTBurks <andrewtburks@gmail.com>
2016-10-20 12:16:06 -0600

    Moved pie over to left side, fixed one text translation, moved legend within the drawing area.

--- src/js/pieToSVG.js
+++ src/js/pieToSVG.js
@@ -19,7 +19,13 @@ function drawPie(starInfo, name, target) {
 	var width = vis.node().clientWidth,
 		height = vis.node().clientHeight;
 
+	var pieCenter = {
+		x: (d3.min([width, height]) / 2),
+		y: (d3.min([width, height]) / 2)
+	};
+
 		console.log(width, height);
+		console.log(center);
 
 	// vis = vis.append("svg").attr("width", "1200").attr("height", "800"); // Added height and width so arc is visible
         var pi = Math.PI;
@@ -40,7 +46,7 @@ function drawPie(starInfo, name, target) {
             .append("path")
             .attr("d", arc)
             .attr("fill", "rgb(253,253,150)")
-            .attr("transform", "translate(" + (width/2) + ", " + (height/2) + ")");
+						.attr("transform", "translate(" + (pieCenter.x) + ", " + (pieCenter.y) + ")");
         }
         for (var i = 0; i < length; i++) {
             var arc = d3.arc()
@@ -53,7 +59,7 @@ function drawPie(starInfo, name, target) {
             .append("path")
             .attr("d", arc)
             .attr("fill", "rgb(255,179,71)")
-            .attr("transform", "translate(" + (width/2) + ", " + (height/2) + ")");
+						.attr("transform", "translate(" + (pieCenter.x) + ", " + (pieCenter.y) + ")");
         }
         for (var i = 0; i < length; i++) {
 
@@ -62,8 +68,8 @@ function drawPie(starInfo, name, target) {
 
 	    //var angleDiff = ((i * 360 / length + padding) * (pi / 180) - ((i + 1) * 360 / length - padding) * (pi / 180)) / 2;
 	    var radius = ((outerRad - tabIn) - innerRad) / 2 + innerRad;
-	    var transX = (width / 2) + (radius * Math.cos(angle));
-	    var transY = (height / 2) + (radius * Math.sin(angle));
+	    var transX = pieCenter.x + (radius * Math.cos(angle));
+	    var transY = pieCenter.y + (radius * Math.sin(angle));
 
             vis
 	    .append("text")
@@ -80,11 +86,11 @@ function drawPie(starInfo, name, target) {
         .append("path")
         .attr("d", center)
         .attr("fill", "rgb(255,105,97)")
-				.attr("transform", "translate(" + (width/2) + ", " + (height/2) + ")");;
+				.attr("transform", "translate(" + (pieCenter.x) + ", " + (pieCenter.y) + ")");
 
         vis
 	.append("text")
-	.attr("transform", "translate(" + (width/2) + ", " + (5 + height/2) + ")")
+	.attr("transform", "translate(" + (pieCenter.x) + ", " + (5 + pieCenter.y) + ")")
 	.style("text-anchor", "middle")
 	.text(name);
         for (var i = 0; i < length; i++) {
@@ -103,7 +109,7 @@ function drawPie(starInfo, name, target) {
                 .append("path")
                 .attr("d", arc)
                 .attr("fill", "rgb(177,156,217)")
-		            .attr("transform", "translate(" + (width/2) + ", " + (height/2) + ")");;
+								.attr("transform", "translate(" + (pieCenter.x) + ", " + (pieCenter.y) + ")");
 
 		var tabText = starInfo.planets[i].pl_orbper;
 
@@ -111,10 +117,10 @@ function drawPie(starInfo, name, target) {
 
 		var angle = arcRangeStart + (j * trisectRange + nestPadding) * (pi / 180) - pi/2 + bisectRange;
 
-		var transX1 = ((width / 2) + ((outerRad - tabIn) * Math.cos(angle) ) )
-		var transY1 = ((height / 2) + ((outerRad - tabIn) * Math.sin(angle) ) )
-		var transX2 = ((width / 2) + ((outerRad + tabOut) * Math.cos(angle) ) )
-		var transY2 = ((height / 2) + ((outerRad + tabOut) * Math.sin(angle) ) )
+		var transX1 = (pieCenter.x  + ((outerRad - tabIn) * Math.cos(angle) ) )
+		var transY1 = (pieCenter.y + ((outerRad - tabIn) * Math.sin(angle) ) )
+		var transX2 = (pieCenter.x  + ((outerRad + tabOut) * Math.cos(angle) ) )
+		var transY2 = (pieCenter.y + ((outerRad + tabOut) * Math.sin(angle) ) )
 
 		if((angle * (180/pi)) % 360 > 270 || (angle * (180/pi)) % 360 <= 90)
             		vis
@@ -138,7 +144,7 @@ function drawPie(starInfo, name, target) {
                 .append("path")
                 .attr("d", arc)
                 .attr("fill", "rgb(174,198,207)")
-		            .attr("transform", "translate(" + (width/2) + ", " + (height/2) + ")");
+								.attr("transform", "translate(" + (pieCenter.x) + ", " + (pieCenter.y) + ")");
 
 		var tabText = starInfo.planets[i].pl_msinie;
 
@@ -146,10 +152,10 @@ function drawPie(starInfo, name, target) {
 
 		var angle = arcRangeStart + (j * trisectRange + nestPadding) * (pi / 180) - pi/2 + bisectRange;
 
-		var transX1 = ((width / 2) + ((outerRad - tabIn) * Math.cos(angle) ) )
-		var transY1 = ((height / 2) + ((outerRad - tabIn) * Math.sin(angle) ) )
-		var transX2 = ((width / 2) + ((outerRad + tabOut) * Math.cos(angle) ) )
-		var transY2 = ((height / 2) + ((outerRad + tabOut) * Math.sin(angle) ) )
+		var transX1 = (pieCenter.x + ((outerRad - tabIn) * Math.cos(angle) ) )
+		var transY1 = (pieCenter.y + ((outerRad - tabIn) * Math.sin(angle) ) )
+		var transX2 = (pieCenter.x  + ((outerRad + tabOut) * Math.cos(angle) ) )
+		var transY2 = (pieCenter.y + ((outerRad + tabOut) * Math.sin(angle) ) )
 
 		if((angle * (180/pi)) % 360 > 270 || (angle * (180/pi)) % 360 <= 90)
             		vis
@@ -173,7 +179,7 @@ function drawPie(starInfo, name, target) {
                 .append("path")
                 .attr("d", arc)
                 .attr("fill", "rgb(119,221,119)")
-		            .attr("transform", "translate(" + (width/2) + ", " + (height/2) + ")");
+								.attr("transform", "translate(" + (pieCenter.x) + ", " + (pieCenter.y) + ")");
 
 		var tabText = starInfo.planets[i].pl_discmethod;
 
@@ -181,10 +187,10 @@ function drawPie(starInfo, name, target) {
 
 		var angle = arcRangeStart + (j * trisectRange + nestPadding) * (pi / 180) - pi/2 + bisectRange;
 
-		var transX1 = (400 + ((outerRad - tabIn) * Math.cos(angle) ) )
-		var transY1 = (400 + ((outerRad - tabIn) * Math.sin(angle) ) )
-		var transX2 = (400 + ((outerRad + tabOut) * Math.cos(angle) ) )
-		var transY2 = (400 + ((outerRad + tabOut) * Math.sin(angle) ) )
+		var transX1 = (pieCenter.x + ((outerRad - tabIn) * Math.cos(angle) ) )
+		var transY1 = (pieCenter.y + ((outerRad - tabIn) * Math.sin(angle) ) )
+		var transX2 = (pieCenter.x + ((outerRad + tabOut) * Math.cos(angle) ) )
+		var transY2 = (pieCenter.y + ((outerRad + tabOut) * Math.sin(angle) ) )
 
 		if((angle * (180/pi)) % 360 > 270 || (angle * (180/pi)) % 360 <= 90)
             		vis
@@ -204,10 +210,11 @@ function drawPie(starInfo, name, target) {
 }
 
 function drawLegend(d, i, svg) {
-    var xOff = 700; var yOff = 100;
+    var xOff = svg.node().clientWidth - 200;
+		var yOff = 10;
     svg.append('rect').attr("x",xOff).attr("y",yOff + i * 30).attr("width",20).attr("height",15).style("fill",d.color);
-    svg.append('text').attr("x",xOff + 30).attr("y",yOff + i * 30 + 15)
+    svg.append('text').attr("x",xOff + 30).attr("y",yOff + i * 30 + 12)
     .attr("font-family", "sans-serif")
-    .attr("font-size", "15px")
+    .attr("font-size", "12px")
     .text(d.name);
 }
